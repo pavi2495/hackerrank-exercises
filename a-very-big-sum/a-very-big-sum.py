b4685edab99b16742c9d98ee490f99ac363c9f57
pavithra <kspavithra2495@gmail.com>
2020-04-01 14:31:03 +0200

    big sum

---
+++ a-very-big-sum/a-very-big-sum.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+
+    count = 0
+    arl = len(ar)
+    for i in range(arl):
+
+        count = count + ar[i]
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
